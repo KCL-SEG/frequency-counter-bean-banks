f749d665c1b77b12f4b01a2a9ce35e2508ad223c
Ben Banks <benjamin.banks960@gmail.com>
2022-09-29 14:55:57 +0100

    Implement frequency function

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in items:
+        item = str(item)
+        if not frequencies.get(item, None):
+            frequencies.update({item: 1})
+        else:
+            frequencies.update({item: frequencies.get(item)+1})
     # Your code goes here
     return frequencies
